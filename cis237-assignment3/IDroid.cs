5a76c478271f0e87d4d1b9e36f631b99e59d9aad
David Barnes <barnesdavidj@gmail.com>
2018-09-03 09:43:27 -0400

    Initial Commit.

---
+++ cis237-assignment3/IDroid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cis237_assignment3
+{
+    interface IDroid
+    {
+        // Method to calculate the total cost of a droid
+        void CalculateTotalCost();
+
+        // Property to get the total cost of a droid
+        decimal TotalCost { get; set; }
+    }
+}
